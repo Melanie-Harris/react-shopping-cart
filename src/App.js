9c20ff996a44fe73eab8810eb1b4289bb0898bae
Melanie Harris <mell.j.harris@gmail.com>
2019-07-22 14:55:23 -0700

    Step 3 and 4 completed. Created additional context, added imports

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import data from './data';
 
 // Components
 import Navigation from './components/Navigation';
-import Products from './components/Products';
+// import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -18,6 +19,7 @@ function App() {
 
 	return (
 		<div className="App">
+			<CartContext.Provider value={cart}>
 			<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
@@ -30,6 +32,7 @@ function App() {
 			/>
 
 			</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
